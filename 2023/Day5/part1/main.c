f54ed4f19a3508f45a40f9b1a4d1967ee218e613
Almog Dobrescu <dobalmog123@gmail.com>
2024-06-11 15:15:20 +0200

    2023 Day 5 part 1 Start

---
+++ 2023/Day5/part1/main.c
@@ -0,0 +1,142 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+#define MAX_LEN_LINE (int)1e3
+#define MAXDIR 100
+#define dprintSTRING(expr) printf(#expr " = %s\n", expr)
+#define dprintCHAR(expr) printf(#expr " = %c\n", expr)
+#define dprintINT(expr) printf(#expr " = %d\n", expr)
+#define dprintUINT(expr) printf(#expr " = %u\n", expr)
+
+int get_line(FILE *fp, char *dst);
+int length(char *str);
+int get_next_word_from_line(char *dst, char *src);
+void copy_arry_by_indesies(char *target, int start, int end, char *src);
+int get_word_and_cut(char *dst, char *src);
+
+int main(int argc, char const *argv[])
+{
+    char input_dir[MAXDIR];
+    FILE *fp;
+
+    if (--argc != 1) {
+        fprintf(stderr, "ERROR: not right usage\n");
+        return -1;
+    }
+    strncpy(input_dir, (*(++argv)), MAXDIR);
+
+    fp = fopen(input_dir, "r");
+    if (fp == NULL) {
+        fprintf(stderr, "ERROR: couldn't find file path\n");
+        return -1;
+    }
+
+/*------------------------------------------------------------*/
+    
+    return 0;
+}
+
+int get_line(FILE *fp, char *dst)
+{
+    int i = 0;
+    char c;
+
+    while ((c = fgetc(fp)) != '\n' && c != EOF) {
+        dst[i] = c;
+        i++;
+        if (i >= MAX_LEN_LINE) {
+            fprintf(stderr, "ERROR: line too long\n");
+            return -1;
+        }
+    }
+    dst[i] = '\0';
+    if (i == 0) {
+        return -1;
+    }
+    return i;
+}
+
+int length(char *str)
+{
+    char c;
+    int i = 0;
+
+    while ((c = str[i]) != '\0') {
+        i++;
+    }
+    return i++;
+}
+
+int get_next_word_from_line(char *dst, char *src)
+{
+    int i = 0, j = 0;
+    char c;
+
+    while (isspace((c = src[i]))) {
+        i++;
+    }
+
+    while ((c = src[i]) != ' ' &&
+                      c != ';' &&
+                      c != ':' &&
+                      c != ',' &&
+                      c != '\n'&&
+                      c != '\0') {
+                        dst[j] = src[i];
+                        i++;
+                        j++;
+    }
+
+    if ((c == ' ' ||
+        c == ';' ||
+        c == ':' ||
+        c == ',' ||
+        c == '\n'||
+        c == '\0') && i == 0) {
+            dst[j++] = c;
+            i++;
+    }
+
+    dst[j] = '\0';
+
+    /*test*/
+    // dprintCHAR(src[i]);
+    /*test*/
+    if (src[i] == '\0') {
+        return -1;
+    }
+    return i;
+
+}
+
+void copy_arry_by_indesies(char *target, int start, int end, char *src)
+{
+    int j = 0;
+    for (int i = start; i < end; i++) {
+        target[j] = src[i];
+        j++;
+    }
+    target[j] = '\0';
+}
+
+int get_word_and_cut(char *dst, char *src)
+{
+    int last_pos;
+
+    if (src[0] == '\0') {
+        return 0;
+    }
+    last_pos = get_next_word_from_line(dst, src);
+    if (last_pos == -1) {
+        return 0;
+    }
+    /*test*/
+    // dprintSTRING(src);
+    // dprintINT(last_pos);
+    /*test*/
+    copy_arry_by_indesies(src, last_pos, length(src), src);
+    return 1;
+}
+
