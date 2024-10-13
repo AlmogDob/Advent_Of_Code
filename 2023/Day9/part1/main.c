3c74d010105a910df1a23b847eb7810a772b767f
Almog Dobrescu <dobalmog123@gmail.com>
2024-10-13 03:02:43 +0300

    2023 Day 9 part 1 start

---
+++ 2023/Day9/part1/main.c
@@ -0,0 +1,23 @@
+#include <string.h>
+
+int main(int argc, char const *argv[])
+{
+    char input_dir[MAXDIR];
+    FILE *fp;
+
+    if (--argc != 1) {
+        fprintf(stderr, "ERROR: not right usage\n");
+        return -1;
+    }
+    strncpy(input_dir, (*(++argv)), MAXDIR);
+
+    fp = fopen(input_dir, "r");
+    if (fp == NULL) {
+        fprintf(stderr, "ERROR: couldn't find file path\n");
+        return -1;
+    }
+
+/*------------------------------------------------------------*/
+    
+    return 0;
+}
