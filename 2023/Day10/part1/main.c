b5dbc222d68cf75d48fd75fcb62455b6ad40d3b5
Almog Dobrescu <dobalmog123@gmail.com>
2024-10-15 00:12:15 +0300

    2023 Day 10 part 1 start

---
+++ 2023/Day10/part1/main.c
@@ -0,0 +1,26 @@
+#include <string.h>
+#define ALMOG_AOC_IMPLEMENTATION
+#include "Almog_AoC.h"
+
+int main(int argc, char const *argv[])
+{
+    char input_dir[MAXDIR];
+    FILE *fp;
+
+    if (--argc != 1) {
+        fprintf(stderr, "%s:%d: [ERROR]: not right usage\n", __FILE__, __LINE__);
+        return -1;
+    }
+    strncpy(input_dir, (*(++argv)), MAXDIR);
+
+    fp = fopen(input_dir, "r");
+    if (fp == NULL) {
+        fprintf(stderr, "ERROR: couldn't find file path\n");
+        return -1;
+    }
+
+/*------------------------------------------------------------*/
+    printf("Hello, World!\n");
+
+    return 0;
+}
