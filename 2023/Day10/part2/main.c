23518ffda0c59e28080f898a9e1ab93244d1632e
Almog Dobrescu <dobalmog123@gmail.com>
2024-10-19 04:57:17 +0300

    didn't succed

--- 2023/Day10/part2/main.c
+++ 2023/Day10/part2/main.c
@@ -2,6 +2,8 @@
 #define ALMOG_AOC_IMPLEMENTATION
 #include "Almog_AoC.h"
 #include <stdbool.h>
+#include <stdlib.h>
+#include <math.h>
 
 typedef enum {
     GROUND = 0,
@@ -31,6 +33,8 @@ typedef struct {
     bool west;
     bool loop;
     bool dead_end;
+    int x;
+    int y;
 } Tile;
 
 typedef struct {
@@ -38,6 +42,14 @@ typedef struct {
     size_t capacity;
     Tile* elements;
 } ada_Tile_array;
+
+typedef struct {
+    size_t length;
+    size_t capacity;
+    int* elements;
+} ada_int_array;
+
+#define PRINT_INT_ARRAY(a) print_int_array(&a, #a)
 
 int offset2D(size_t x, size_t y, size_t nx);
 void print_symbol_type(Symbol_type st);
@@ -52,6 +64,12 @@ void set_map_conaction(ada_Tile_array *map);
 void init_map(ada_Tile_array *map);
 void find_start(size_t *x, size_t *y, ada_Tile_array *map);
 void next_xy(size_t *next_x, size_t *next_y, ada_Tile_array *map, size_t x, size_t y);
+void print_int_array(ada_int_array *a, char *name);
+void my_qsort_int(int v[], int left, int right);
+void swap_int(int v[], int i, int j);
+int search_x_y(ada_int_array *x_array, ada_int_array *y_array, int x, int y);
+int num_of_inter(ada_int_array *x_array, ada_int_array *y_array, int x, int y, char direction);
+int walking_right(ada_Tile_array *map, int x, int y);
 
 size_t nx, ny;
 
@@ -77,9 +95,12 @@ int main(int argc, char const *argv[])
     char current_line[MAX_LEN_LINE];
     ada_Tile_array map, loop;
     Tile current_tile;
+    ada_int_array loop_x_vals, loop_y_vals;
 
     ada_array(Tile, map);
     ada_array(Tile, loop);
+    ada_array(int, loop_x_vals);
+    ada_array(int, loop_y_vals);
 
     while ((line_len = aaoc_get_line(fp, current_line)) != -1) {
         // dprintSTRING(current_line);
@@ -103,14 +124,51 @@ int main(int argc, char const *argv[])
     ada_appand(Tile, loop, map.elements[offset2D(sx, sy, nx)]);
     next_xy(&next_x, &next_y, &map, sx, sy);
     while ((map.elements[offset2D(next_x, next_y, nx)].symbol != 'S')) {
+        ada_appand(int, loop_x_vals, next_x);
+        ada_appand(int, loop_y_vals, next_y);
         ada_appand(Tile, loop, map.elements[offset2D(next_x, next_y, nx)]);
         next_xy(&next_x, &next_y, &map, next_x, next_y);
     }
+    dprintSIZE_T(nx);
+    dprintSIZE_T(ny);
     print_loop_skinny(&map);
-    
-    print_tile_array(&loop);
 
-    dprintINT((int)((loop.length)/2));
+    PRINT_INT_ARRAY(loop_x_vals);
+    PRINT_INT_ARRAY(loop_y_vals);
+
+    int inside = 0;
+
+    for (int y = 0; y < (int)ny; y++) {
+        for (int x = 0; x < (int)nx; x++) {
+            if (map.elements[offset2D(x, y, nx)].loop == false) {
+                // int up, right, down, left;
+                // // up    = num_of_inter(&loop_x_vals, &loop_y_vals, x, y, 'u');
+                // right = num_of_inter(&loop_x_vals, &loop_y_vals, x, y, 'r');
+                // // down  = num_of_inter(&loop_x_vals, &loop_y_vals, x, y, 'd');
+                // left  = num_of_inter(&loop_x_vals, &loop_y_vals, x, y, 'l');
+                
+                int counter = walking_right(&map, x, y);
+
+
+
+                if (counter % 2 == 1) {
+                    inside++;
+                    printf("I");
+                } else {
+                    printf("O");
+                }
+
+            } else {
+                printf("*");
+            }
+            // break;
+        }
+        // break;
+
+        printf("\n");
+    }
+
+    dprintINT(inside);
 
     return 0;
 }
@@ -538,13 +596,19 @@ void set_map_conaction(ada_Tile_array *map)
 
 void init_map(ada_Tile_array *map)
 {
-    for (size_t i = 0; i < map->length; i++) {
-        set_type(&(map->elements[i]));
-        map->elements[i].north = false;
-        map->elements[i].east = false;
-        map->elements[i].south = false;
-        map->elements[i].west = false;
-        map->elements[i].dead_end = true;
+    for (size_t y = 0; y < ny; y++) {
+        for (size_t x = 0; x < nx; x++) {
+            int i = offset2D(x, y, nx);
+            set_type(&(map->elements[i]));
+            map->elements[i].north = false;
+            map->elements[i].east = false;
+            map->elements[i].south = false;
+            map->elements[i].west = false;
+            map->elements[i].dead_end = true;
+            map->elements[i].loop = false;
+            map->elements[i].x = x;
+            map->elements[i].y = y;
+        } 
     }
 }
 
@@ -733,4 +797,142 @@ void next_xy(size_t *next_x, size_t *next_y, ada_Tile_array *map, size_t x, size
     default:
         break;
     }
+}
+
+void print_int_array(ada_int_array *a, char *name)
+{
+    printf("%s\n", name);
+    printf("[");
+    for (size_t i = 0; i < a->length-1; i++) {
+        printf("%d, ", a->elements[i]);
+    }
+    printf("%d]\n", a->elements[a->length-1]);
+}
+
+/* qsort: sort v[left]...v[right] int increasing order */
+void my_qsort_int(int v[], int left, int right)
+{
+    int i, last;
+
+    if (left >= right)                  /* do nothing if array contains */
+        return;                         /* fewer than two elements */
+    swap_int(v, left, (left + right) / 2);  /* move partition elem */
+    last = left;                        /* to v[0] */
+    for (i = left + 1; i <= right; i++) /* partition */
+        if (v[i] < v[left])  
+            swap_int(v, ++last, i);
+    swap_int(v, left, last); /* restore partition elem */
+    my_qsort_int(v, left, last - 1);
+    my_qsort_int(v, last + 1, right);
+}
+
+/* swap: interchange v[i] and v[j] */
+void swap_int(int v[], int i, int j)
+{
+    int temp;
+
+    temp = v[i];
+    v[i] = v[j];
+    v[j] = temp;
+}
+
+int search_x_y(ada_int_array *x_array, ada_int_array *y_array, int x, int y)
+{
+    for (size_t i = 0; i < x_array->length; i++) {
+        if (x_array->elements[i] == x) {
+            if (y_array->elements[i] == y) {
+                return i;
+            }
+        }
+    }
+    return -1;
+}
+
+int num_of_inter(ada_int_array *x_array, ada_int_array *y_array, int x, int y, char direction)
+{
+    int dx = 0, dy = 0, previus_dx = 0, previus_dy = 0, num_of_inter = 0, current_state, previus_state, first = 1;
+
+    if (direction == 'u') {
+        while (y + dy >= 0) {
+            current_state = search_x_y(x_array, y_array, x+dx, y+dy);
+            previus_state = search_x_y(x_array, y_array, x+previus_dx, y+previus_dy);
+            if ((current_state != -1 && previus_state == -1) || (previus_state != -1 && current_state != -1 && abs(previus_state - current_state) != 1)) {
+                num_of_inter++;
+            }
+            if (dy != 0) {
+                previus_dy--;
+            }
+            dy--;
+        }
+    } else if (direction == 'd') {
+        while (y + dy < ny) {
+            current_state = search_x_y(x_array, y_array, x+dx, y+dy);
+            previus_state = search_x_y(x_array, y_array, x+previus_dx, y+previus_dy);
+            if ((current_state != -1 && previus_state == -1) || (previus_state != -1 && current_state != -1 && abs(previus_state - current_state) != 1)) {
+                num_of_inter++;
+            }
+            // if (first && (previus_state != -1 && current_state != -1 && abs(previus_state - current_state) == 1)) {
+            //     num_of_inter++;
+            //     first = 0;
+            // }
+            if (dy != 0) {
+                previus_dy++;
+            }
+            dy++;
+        }
+    } else if (direction == 'r') {
+        while (x + dx < nx) {
+            current_state = search_x_y(x_array, y_array, x+dx, y+dy);
+            previus_state = search_x_y(x_array, y_array, x+previus_dx, y+previus_dy);
+            if ((current_state != -1 && previus_state == -1) || (previus_state != -1 && current_state != -1 && abs(previus_state - current_state) != 1)) {
+                num_of_inter++;
+            }
+            // if (first && (previus_state != -1 && current_state != -1 && abs(previus_state - current_state) == 1)) {
+            //     num_of_inter++;
+            //     first = 0;
+            // }
+            // printf("dy   : %3d, pre dy: %3d\n", dx, previus_dx);
+            // printf("cu st: %3d, pre st: %3d\n", current_state, previus_state);
+            // printf("inter: %d\n", num_of_inter);
+            if (dx!= 0) {
+                previus_dx++;
+            }
+            dx++;
+        }
+    } else if (direction == 'l') {
+        while (x + dx >= 0) {
+            current_state = search_x_y(x_array, y_array, x+dx, y+dy);
+            previus_state = search_x_y(x_array, y_array, x+previus_dx, y+previus_dy);
+            if ((current_state != -1 && previus_state == -1) || (previus_state != -1 && current_state != -1 && abs(previus_state - current_state) != 1)) {
+                num_of_inter++;
+            }
+            if (first && (previus_state != -1 && current_state != -1 && abs(previus_state - current_state) == 1)) {
+                num_of_inter++;
+                first = 0;
+            }
+            // printf("dy   : %3d, pre dy: %3d\n", dx, previus_dx);
+            // printf("cu st: %3d, pre st: %3d\n", current_state, previus_state);
+            // printf("inter: %d\n", num_of_inter);
+            if (dx != 0) {
+                previus_dx--;
+            }
+            dx--;
+        }
+    }
+    return num_of_inter;
+}
+
+int walking_right(ada_Tile_array *map, int x, int y)
+{
+    int dx = 0, count_north = 0, count_south = 0;
+
+    while (dx + x < nx) {
+        if (map->elements[offset2D(x+dx, y, nx)].type == NORTH_WEST || map->elements[offset2D(x+dx, y, nx)].type == NORTH_EAST || map->elements[offset2D(x+dx, y, nx)].type == NORTH_SOUTH) { count_north++;
+        }
+        if (map->elements[offset2D(x+dx, y, nx)].type == SOUTH_EAST || map->elements[offset2D(x+dx, y, nx)].type == SOUTH_WEST || map->elements[offset2D(x+dx, y, nx)].type == NORTH_SOUTH) {
+            count_south++;
+        }
+        dx++;
+    }
+    return (int)fmin(count_north, count_south);
 }
